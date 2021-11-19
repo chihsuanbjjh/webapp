11df59fa5f1f7ec31d380b45eb550fcf1c3499a6
Chihsuan <chihsuan@bjjh.tp.edu.tw>
2021-11-20 06:00:34 +0800

    update

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+// js連結網頁的元件
+
 var title = document.getElementById("title");
 var content = document.getElementById("content");
 var btn = document.getElementById("btn");
 var list = document.getElementById("list");
 
 // 按下按鈕後，更換標題與內文，清空上方的標題及內文
+// btn.addEventListener(動作,函式);
 // btn.addEventListener("click" submit());
 btn.addEventListener("click",change);
 
 function change(){
-  list.innerHTML = list.innerHTML + `
+  // 引用變數的方式是${變數名}
+  list.innerHTML = list.innerHTML +`
   	<div class="article">
         <h2>${title.value}</h2>
         <p>${content.value}</p>
