0fa75c61eaa854c74eecddbac0effde21b6436d5
Chihsuan <chihsuan@bjjh.tp.edu.tw>
2022-04-15 23:05:16 +0800

    UP

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,15 +1,12 @@
 // // js連結網頁的元件
-
 // var title = document.getElementById("title");
 // var content = document.getElementById("content");
 // var btn = document.getElementById("btn");
 // var list = document.getElementById("list");
-
 // // 按下按鈕後，更換標題與內文，清空上方的標題及內文
 // // btn.addEventListener(動作,函式);
 // // btn.addEventListener("click" submit());
 // btn.addEventListener("click",change);
-
 // function change(){
 //   // 引用變數的方式是${變數名}
 //   list.innerHTML = list.innerHTML +`
@@ -18,6 +15,6 @@
 //         <p>${content.value}</p>
 //       </div>
 //   `;
-
 //   console.log(title.value);
 // }
+"use strict";
